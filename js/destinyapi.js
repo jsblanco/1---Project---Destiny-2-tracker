e1146dd04903ca42299ce961deef534419465fd6
Jorge Sb <jorgesanchezblanco1989@gmail.com>
2020-02-29 20:57:11 +0100

    Added search functionality for all platforms

--- js/destinyapi.js
+++ js/destinyapi.js
@@ -1,31 +1,3 @@
-var apiKey = "dd6e865e28924fad9ea265dfae890e35";
-
-const searchButton = document.getElementById("search-button")
-const searchInput = document.getElementById("steam-name-input")
-const resultsList = document.getElementById("steam-results")
-const queryStatus = document.getElementById("status")
-
-/*
-let membershipType= 3
-let destinyMembershipId= "4611686018467782694";*/
-
-searchButton.onclick = () => {
-  resultsList.innerHTML = ""
-  let user = searchInput.value
-  if (user === "")
-  {pleaseInputAccountName= document.createElement("h3")
-  pleaseInputAccountName.setAttribute("class", "text-danger w-100 font-weight-bold mx-3 text-center")
-  pleaseInputAccountName.innerHTML="Por favor, incluye un nombre de cuenta válido"
-  resultsList.appendChild(pleaseInputAccountName);} else {
-  //console.log("Búsqueda se inicia")
-  steamDisplayNameQuery()}
-}
-
-
-function inputValidator(){
-  
-}
-
 /*
 //INFO DE LA API:
 //https://bungie-net.github.io/multi/operation_get_User-SearchUsers.html#operation_get_User-SearchUsers
@@ -33,23 +5,65 @@ function inputValidator(){
 //https://bungie-net.github.io/multi/schema_Destiny-DestinyComponentType.html
 //Components are added after the fetch URL, inside the ticks, like this: /?components=200 
 My profileId: 4611686018467782694
-
-fetch("https://www.bungie.net/Platform//User/SearchUsers/?q=Cydonia", {
-headers: {"X-API-Key": apiKey}})
-.then (response => response.json())
-.then (json => console.log(json))
+Steam membershipType: 3
 */
+
+var apiKey = "dd6e865e28924fad9ea265dfae890e35";
+
+const searchSteam = document.getElementById("platform-steam")
+const searchPlaystation = document.getElementById("platform-playstation")
+const searchXbox = document.getElementById("platform-xbox")
+const searchInput = document.getElementById("username-input")
+const resultsList = document.getElementById("steam-results")
+const queryStatus = document.getElementById("status")
+
+
+searchSteam.onclick = () => {
+  //console.log("Búsqueda de Steam se inicia")
+  resultsList.innerHTML = ""
+  isInputValid()? steamDisplayNameQuery():invalidInput()
+}
+
+searchPlaystation.onclick = () => {
+  //console.log("Búsqueda de Steam se inicia")
+  resultsList.innerHTML = ""
+  isInputValid()? playstationDisplayNameQuery():invalidInput()
+}
+
+searchXbox.onclick = () => {
+  //console.log("Búsqueda de Steam se inicia")
+  resultsList.innerHTML = ""
+  isInputValid()? xBoxDisplayNameQuery():invalidInput()
+}
+
+
+
+function isInputValid(){
+  if (searchInput.value != "") {
+    return true
+  } else {
+    
+    return false}
+  }
+
+function invalidInput(){
+pleaseInputAccountName= document.createElement("h3")
+pleaseInputAccountName.setAttribute("class", "text-danger w-100 font-weight-bold mx-3 text-center")
+pleaseInputAccountName.innerHTML="Por favor, incluye un nombre de cuenta válido"
+resultsList.appendChild(pleaseInputAccountName);
+}
+
 
 //Estas funciones nos añaden un mensaje de Cargando, y luego los eliminan o sustituyen a medida que encuentran usuarios y personajes
 
 function addLoadingMessage(){
   selectYourAccount= document.createElement("h4")
   selectYourAccount.setAttribute("id", "loadingResults")
-  selectYourAccount.setAttribute("class", "w-100 font-weight-bold")
+  selectYourAccount.setAttribute("class", "w-100 font-weight-bold mx-3")
   selectYourAccount.innerHTML=`Cargando los resultados...`
   noResults=document.createElement("p")
   noResults.setAttribute("id", "noResults")
-  noResults.setAttribute("class", "w-100 font-weight-bold text-muted")
+  noResults.setAttribute("class", "w-100 font-weight-bold text-muted mx-3")
   noResults.innerHTML=`<i>¿No aparecen resultados? Prueba a reformular tu búsqueda</i>`
   queryStatus.appendChild(selectYourAccount);
   queryStatus.appendChild(noResults);
@@ -58,7 +72,7 @@ function addLoadingMessage(){
 function usersFound(){
   let loadingMessage = document.querySelector('#loadingResults');
   let noResults = document.querySelector('#noResults');
-  loadingMessage.innerHTML="Cuentas de Steam registradas en Bungie:";
+  loadingMessage.innerHTML="Cuentas registradas en Bungie para esta plataforma:";
   noResults.parentNode.removeChild(noResults);
 }
 
@@ -69,33 +83,88 @@ function charactersFound(){
 
 
 //COMIENZA LO DIVERTIDO
+//Haremos tres funciones distintas, una por plataforma. 
+
 //Aquí consigo un array con todos los usuarios de Steam. 
 //Lo usaremos para extraer sus personajes y sus iconos mediante su membershipId
 
 function steamDisplayNameQuery() {
 //log// console.log("Empieza steamDisplayNameQuery")
-  let user = searchInput.value
-  queryStatus.innerHTML=""
-  resultsList.innerHTML = ""
-  addLoadingMessage()
-  fetch(`https://www.bungie.net/Platform/User/SearchUsers/?q=${user}`, {
-      headers: {
-        "X-API-Key": apiKey
-      }
-    })
-    .then(response => response.json())
-    .then(json => json.Response)
-    .then(function (players) {
-       return players.filter(function (player) {
+let user = searchInput.value
+    queryStatus.innerHTML=""
+    resultsList.innerHTML = ""
+    addLoadingMessage()
+    fetch(`https://www.bungie.net/Platform/User/SearchUsers/?q=${user}`, {
+        headers: {
+          "X-API-Key": apiKey
+        }
+      })
+      .then(response => response.json())
+      .then(json => json.Response)
+      .then(function (players) {
+        return players.filter(function (player) {
           return (player.steamDisplayName);
-          }
-        );
-      }
-    )
-//Si da tiempo a ampliar la búsqueda de Steam a cualquier plataforma, deberíamos cambiar este 3 por el de la pataforma escogida (mediante dropdown?) 
-.then(steamPlayers => membershipByPlatform(steamPlayers, 3))
+        }
+      )
+    }
+  )
+  .then ((steamPlayers) => membershipByPlatform(steamPlayers, 3))
 //log//.then (()=>console.log("Termina steamDisplayNameQuery"))
 }
+
+function playstationDisplayNameQuery() {
+  //log// console.log("Empieza steamDisplayNameQuery")
+    let user = searchInput.value
+    queryStatus.innerHTML=""
+    resultsList.innerHTML = ""
+    addLoadingMessage()
+    fetch(`https://www.bungie.net/Platform/User/SearchUsers/?q=${user}`, {
+        headers: {
+          "X-API-Key": apiKey
+        }
+      })
+      .then(response => response.json())
+      .then(json => json.Response)
+      .then(function (players) {
+         return players.filter(function (player) {
+            return (player.psnDisplayName);
+            }
+          );
+        }
+      )
+
+  .then(playStationPlayers => membershipByPlatform(playStationPlayers, 1))
+  //log//.then (()=>console.log("Termina steamDisplayNameQuery"))
+  }
+
+  function xBoxDisplayNameQuery() {
+    //log// console.log("Empieza steamDisplayNameQuery")
+      let user = searchInput.value
+      queryStatus.innerHTML=""
+      resultsList.innerHTML = ""
+      addLoadingMessage()
+      fetch(`https://www.bungie.net/Platform/User/SearchUsers/?q=${user}`, {
+          headers: {
+            "X-API-Key": apiKey
+          }
+        })
+        .then(response => response.json())
+        .then(json => json.Response)
+        .then(function (players) {
+           return players.filter(function (player) {
+              return (player.xboxDisplayName);
+              }
+            );
+          }
+        )
+  
+    .then(xBoxPlayers => membershipByPlatform(xBoxPlayers, 2))
+    //log//.then (()=>console.log("Termina steamDisplayNameQuery"))
+    }
+
+
+
+
 
 //Aquí consigues el membershipID específico de Destiny 2
 //Tenemos que meter steamPlayers.membershipId
@@ -131,14 +200,13 @@ function populateResults(json) {
     newResult.setAttribute("class", "col-xl-3 col-lg-4 col-md-6 col-xs-8")
     let resultHeader = document.createElement("li")
     resultHeader.setAttribute("class", "userHeader")
-    resultHeader.innerHTML = `Nombre en Steam: <b>${json[i].displayName}</b>`
+    resultHeader.innerHTML = `<b>${json[i].displayName}</b>`
     //+` Identificador de Bungie: <b>${json[i].membershipId}</b>`
     resultsList.appendChild(newResult);
     newResult.appendChild(resultHeader);
     getPlayerCharacters(json[i])
   }
 usersFound()
-
 }
 
 //Esta función nos consigue el perfil de Destiny de cada jugador en base al membershipId que obtuvimos en membershipByPlatform, via populateResults
@@ -225,3 +293,36 @@ function populateCharacterInfo(characterId) {
   userUl.appendChild(spentTimeLi)
 }
 
+
+
+/* Consultar con los TAs: ¿Por qué me dice que usernameFetch() no es una función?
+
+
+function usernameFetch(){
+let user = searchInput.value
+queryStatus.innerHTML=""
+resultsList.innerHTML = ""
+addLoadingMessage()
+fetch(`https://www.bungie.net/Platform/User/SearchUsers/?q=${user}`, {
+    headers: {
+      "X-API-Key": apiKey
+    }
+  })
+  .then(response => response.json())
+  .then(json => json.Response)
+}
+
+
+function steamDisplayNameQuery() {
+  new Promise (usernameFetch())
+  .then(function (players) {
+    return players.filter(function (player) {
+    return (player.steamDisplayName);
+        }
+      )
+    }
+  )
+  .then ((steamPlayers) => membershipByPlatform(steamPlayers, 3))
+}
+
+*/
